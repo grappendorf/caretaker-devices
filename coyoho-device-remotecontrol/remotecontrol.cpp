2c40d0c57948cc040c444b2bbed7fd5ccde32f20
Dirk Grappendorf <d.grappendorf@web.de>
2012-06-12 13:54:38 +0200

    Multiple button pages Option to send notifications to listeners

--- coyoho-device-remotecontrol/remotecontrol.cpp
+++ coyoho-device-remotecontrol/remotecontrol.cpp
@@ -30,13 +30,14 @@
  */
 
 #include <Arduino.h>
-#include <PinChangeInt/PinChangeInt.h>
-#include <XXBee/XXBee.h>
-#include <CoYoHoMessages.h>
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/io.h>
 #include <avr/eeprom.h>
+#include <PinChangeInt/PinChangeInt.h>
+#include <XXBee/XXBee.h>
+#include <CoYoHoMessages.h>
+#include <CoYoHoListenerManager.h>
 
 /** IO Pins */
 const uint8_t RXD = 0;
@@ -57,55 +58,163 @@ const uint8_t BUTTON_09 = 10;
 const uint8_t BUTTON_10 = 9;
 const uint8_t LOW_BAT = 8;
 
+/** XBee device */
+XXBee<8> xbee;
+ZBTxStatusResponse txStatusResponse;
+ZBRxResponse rxResponse;
+const long XBEE_BAUD_RATE = 57600;
+
+/** Listeners */
+ListenerManager<4> listenerManager(& xbee);
+
+/** Some LED constants */
 enum Led
 {
 	NONE = 0, YELLOW = 1, GREEN = 2, RED = 4
 };
 
-/** ZigBee baud rate */
-const long XBEE_BAUD_RATE = 57600;
-
-/** XBee device */
-XXBee<8> xbee;
-
-/** Button pins */
+/** Button pin numbers */
 uint8_t buttons[] = { BUTTON_01, BUTTON_02, BUTTON_03, BUTTON_04, BUTTON_05, BUTTON_06, BUTTON_07, BUTTON_08, BUTTON_09,
 		BUTTON_10 };
 
 /** Number of buttons */
-const uint8_t numButtons = sizeof(buttons) / sizeof(buttons[0]);
+const uint8_t NUM_BUTTONS = sizeof(buttons) / sizeof(buttons[0]);
 
-/** XBee TX status response */
-ZBTxStatusResponse txStatusResponse;
+uint8_t lastButtonStates[NUM_BUTTONS];
 
-/** ZigBee reusable RX response message */
-ZBRxResponse rxResponse;
+/** Convenient aliases */
+const uint8_t BUTTON_PRESSED = LOW;
+const uint8_t BUTTON_RELEASED = HIGH;
 
-/** Indicates that no cmommand is defined for a button */
-const uint16_t UNDEF = 0xffff;
+/** Indicates that no command should be defined for a button */
+const uint8_t UNDEFINED = 0xff;
 
-/** Pointers into the code table */
-const uint16_t EEMEM codes[10] = { 0, 12, 24, 36, 48, 60, 72, 84, 99, 114 };
+/** Indicates that listeners should be notified when a button is pressed or released */
+const uint8_t LISTENERS = 0xfe;
 
-/** XBee message codes */
+/**
+ * XBee message codes. First comes the codes that are sent when a button is pressed,
+ * followed by the codes sent when a button is released
+ */
 const uint8_t EEMEM codeData[] = {
-		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 0, COYOHO_WRITE_TOGGLE,
-		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 1, COYOHO_WRITE_TOGGLE,
-		0x00, 0xa2, 0x13, 0x00, 0x97, 0x3d, 0x48, 0x40, 3, COYOHO_PWM_WRITE, 0, COYOHO_WRITE_TOGGLE,
-		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 2, COYOHO_WRITE_TOGGLE,
-		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 3, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_TOGGLE,
-		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 4, COYOHO_WRITE_TOGGLE,
-		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 5, COYOHO_WRITE_TOGGLE,
-		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 6, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_INCREMENT, 32, 0, 0,
-		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 6, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_INCREMENT, 0, 32, 0,
-		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 6, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_INCREMENT, 0, 0, 32,};
 
+		// Page 0: Only notify the listeners
+
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+		LISTENERS,
+
+		// Page 1: Fail back, direct control mode
+
+		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 0, COYOHO_WRITE_TOGGLE,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 1, COYOHO_WRITE_TOGGLE,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x97, 0x3d, 0x48, 0x40, 3, COYOHO_PWM_WRITE, 0, COYOHO_WRITE_TOGGLE,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 2, COYOHO_WRITE_TOGGLE,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 3, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_TOGGLE,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 4, COYOHO_WRITE_TOGGLE,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 3, COYOHO_SWITCH_WRITE, 5, COYOHO_WRITE_TOGGLE,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 6, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_INCREMENT, 32, 0, 0,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 6, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_INCREMENT, 0, 32, 0,
+		UNDEFINED,
+		0x00, 0xa2, 0x13, 0x00, 0x9d, 0x31, 0x61, 0x40, 6, COYOHO_RGB_WRITE, 0, COYOHO_WRITE_INCREMENT, 0, 0, 32,
+		UNDEFINED,
+
+		// Page 2: Experimental mode
+
+		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 4, COYOHO_SWITCH_WRITE, 0, COYOHO_WRITE_ABSOLUTE, 1,
+		0x00, 0xa2, 0x13, 0x00, 0x0c, 0x50, 0x4a, 0x40, 4, COYOHO_SWITCH_WRITE, 0, COYOHO_WRITE_ABSOLUTE, 0,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+		UNDEFINED,
+};
+
+/** Switch beetween various pages of codes */
+uint8_t codeIndexOffset = 0;
+
+const uint8_t NUM_CODE_PAGES = 3;
+
+/**
+ * Pointers into the code tables. Code index for button press at even indices, code index
+ * for button release at odd indices
+ */
+uint16_t codeIndex[NUM_CODE_PAGES * NUM_BUTTONS * 2];
+
+/** Indicates that no button has changed its state since the last check */
+const uint8_t NO_KEY_CHANGED = 0xff;
+
+/** States of the device mode state engine */
 enum State
 {
 	POWER_UP, NORMAL_MODE, PROGRAMMING_MODE
 };
 
+/** Current device mode state */
 State state = POWER_UP;
+
+/**
+ * This function creates a table of pointers into the button code table.
+ * codeIndex then contains the code position for every page, ever button
+ * and each of the pressed and released states.
+ */
+void updateCodeIndices()
+{
+	uint16_t p = 0;
+	for (uint8_t i = 0; i < NUM_CODE_PAGES * NUM_BUTTONS * 2; ++i)
+	{
+		uint8_t buttonType =eeprom_read_byte(codeData + p);
+		if (buttonType == LISTENERS || buttonType == UNDEFINED)
+		{
+			codeIndex[i] = 0xff00 + buttonType;
+			++p;
+		}
+		else
+		{
+			uint8_t length = eeprom_read_byte(codeData + p + 8);
+			codeIndex[i] = p;
+			p += 8 + 1 + length;
+		}
+	}
+}
 
 /**
  * Enable all specified leds.
@@ -120,7 +229,7 @@ void lightLeds(uint8_t leds)
 /**
  * Flash all specified leds.
  */
-void flashLeds(uint8_t leds, uint16_t interval = 200)
+void flashLeds(uint8_t leds, uint16_t interval = 50)
 {
 	lightLeds(leds);
 	delay(interval);
@@ -130,7 +239,7 @@ void flashLeds(uint8_t leds, uint16_t interval = 200)
 /**
  * Flash all specified leds.
  */
-void blinkLeds(uint8_t leds, uint16_t intervalOn = 200, uint16_t intervalOff = 200)
+void blinkLeds(uint8_t leds, uint16_t intervalOn = 50, uint16_t intervalOff = 50)
 {
 	flashLeds(leds, intervalOn);
 	delay(intervalOff);
@@ -170,15 +279,31 @@ void checkLowBattery()
 }
 
 /**
+ * Notify all registered listeners about a button state change.
+ */
+void notifyListeners(uint8_t button, uint8_t buttonState)
+{
+	uint8_t message[] = { COYOHO_SWITCH_READ | COYOHO_MESSAGE_NOTIFY, button,
+			buttonState == BUTTON_PRESSED ? 1 : 0 };
+	listenerManager.notifyListeners(message, sizeof(message));
+}
+
+/**
  * Send the XBee message of the specifed button.
  */
-void sendMessageOfButton(uint8_t button)
+void sendMessageOfButton(uint8_t button, uint8_t buttonState)
 {
-	uint16_t c = eeprom_read_word(codes + button);
-	if (c != UNDEF)
+	uint16_t c = codeIndex[codeIndexOffset + button * 2 + (buttonState == BUTTON_PRESSED ? 0 : 1)];
+	if (c == 0xff00 + LISTENERS)
 	{
 		flashLeds(YELLOW);
-
+		notifyListeners(button, buttonState);
+		flashLeds(GREEN);
+		waitForXBeeSendFinished();
+	}
+	else if (c != 0xff00 + UNDEFINED)
+	{
+		flashLeds(YELLOW);
 		uint32_t addrHi = eeprom_read_dword((uint32_t *) (codeData + c));
 		c += 4;
 		uint32_t addrLo = eeprom_read_dword((uint32_t *) (codeData + c));
@@ -194,7 +319,6 @@ void sendMessageOfButton(uint8_t button)
 		}
 		ZBTxRequest txRequest(addr, xbee.payload(), xbee.payloadLenght());
 		xbee.send(txRequest);
-
 		boolean ok = false;
 		if (xbee.readPacket(2000))
 		{
@@ -207,38 +331,41 @@ void sendMessageOfButton(uint8_t button)
 				}
 			}
 		}
-
 		flashLeds(ok ? GREEN : RED);
 		waitForXBeeSendFinished();
 	}
 }
 
 /**
- * Return the number of the pressed button (0..9) or -1 if none was pressed.
- * @return
+ * Return the number of the pressed button (0..9) or NO_KEY_CHANEGD if none was pressed.
  */
-int8_t checkButtons()
+uint8_t checkButtons()
 {
-	for (uint8_t i = 0; i < numButtons; ++i)
+	uint8_t changedKey = NO_KEY_CHANGED;
+	for (uint8_t i = 0; i < NUM_BUTTONS; ++i)
 	{
-		if (digitalRead(buttons[i]) == LOW)
+		uint8_t buttonState = digitalRead(buttons[i]);
+		if (buttonState != lastButtonStates[i])
 		{
-			return i;
+			lastButtonStates[i] = buttonState;
+			changedKey = i;
 		}
 	}
-	return -1;
+	return changedKey;
 }
 
 /**
- * Check if any buttons are pressed and react to it, i.e. send the programmed
- * XBee messages.
+ * As long as the state of any button changes, sent the assigned XBee messages.
+ * A button change can change during this message sending, so we must loop and only
+ * return if there was no more change.
  */
-void checkAndReactToButtons()
+void checkAndReactToButtonChanges()
 {
-	int8_t button = checkButtons();
-	if (button != -1)
+	uint8_t changedButton = checkButtons();
+	while (changedButton != NO_KEY_CHANGED)
 	{
-		sendMessageOfButton(button);
+		sendMessageOfButton(changedButton, lastButtonStates[changedButton]);
+		changedButton = checkButtons();
 	}
 }
 
@@ -262,22 +389,17 @@ void checkAndReactToIncomingXBeeMesage()
 				switch (command)
 				{
 					case COYOHO_PROGRAM_WRITE:
-						if (xbee.dataAvailable(20))
+					{
+						uint8_t *addr = (uint8_t *) codeData;
+						while (xbee.dataAvailable())
 						{
-							uint8_t *addr = (uint8_t *) codes;
-							for (uint8_t i = 0; i < 20; ++i)
-							{
-								eeprom_write_byte(addr++, xbee.getData());
-							}
-							addr = (uint8_t *) codeData;
-							while (xbee.dataAvailable())
-							{
-								eeprom_write_byte(addr++, xbee.getData());
-							}
-							flashLeds(RED | GREEN | YELLOW);
-							flashLeds(RED | GREEN | YELLOW);
+							eeprom_write_byte(addr++, xbee.getData());
 						}
+						updateCodeIndices();
+						flashLeds(RED | GREEN | YELLOW);
+						flashLeds(RED | GREEN | YELLOW);
 						break;
+					}
 
 					case COYOHO_SWITCH_WRITE:
 						if (xbee.dataAvailable(2))
@@ -290,7 +412,8 @@ void checkAndReactToIncomingXBeeMesage()
 								{
 									default:
 										// Every switch message type simulates a button press
-										sendMessageOfButton(switchNum);
+										sendMessageOfButton(switchNum, BUTTON_PRESSED);
+										sendMessageOfButton(switchNum, BUTTON_RELEASED);
 										break;
 								}
 							}
@@ -306,18 +429,25 @@ void checkAndReactToIncomingXBeeMesage()
 }
 
 /**
- * System setup.
+ * Main system setup.
  */
 void setup()
 {
+	// TODO: Since the remote control device is in deep sleep mode all the time, we cannot listen
+	// to incoming listener requests (for this to happen we need to enable wake up on UART).
+	// To send listener messages to the server, here we hard coded the listeners.
+	listenerManager.addListener(XBeeAddress64(0x0013a200, 0x406131ae));
+	listenerManager.addListener(XBeeAddress64(0x0013a200, 0x404a500b));
+	updateCodeIndices();
 	xbeeSleep(true);
 	pinMode(XBEE_SLEEP, OUTPUT);
 	lightLeds(NONE);
 	pinMode(LED_RED, OUTPUT);
 	pinMode(LED_GREEN, OUTPUT);
 	pinMode(LED_YELLOW, OUTPUT);
-	for (uint8_t i = 0; i < numButtons; ++i)
+	for (uint8_t i = 0; i < NUM_BUTTONS; ++i)
 	{
+		lastButtonStates[i] = BUTTON_RELEASED;
 		digitalWrite(buttons[i], HIGH);
 		pinMode(buttons[i], INPUT);
 		PCattachInterrupt(buttons[i], buttonPressed, RISING);
@@ -332,25 +462,44 @@ void setup()
 }
 
 /**
- * Main execution loop.
+ * Main system loop.
  */
 void loop()
 {
+	// TODO: Currently we don't check the listener leases. See the comment about listeners
+	// in the setup function.
+	//listenerManager.checkListenerLeases();
 	checkLowBattery();
 	switch (state)
 	{
 		case POWER_UP:
-			if (checkButtons() != -1)
+		{
+			uint8_t button = checkButtons();
+			if (button != NO_KEY_CHANGED)
 			{
-				blinkLeds(RED | GREEN | YELLOW);
-				xbeeSleep(false);
-				state = PROGRAMMING_MODE;
+				for (uint8_t i = 0; i <= button; ++i)
+				{
+					blinkLeds(RED | GREEN | YELLOW);
+				}
+				if (button == 9)
+				{
+					// Pressing button 10 when powering up, enter programming mode
+					xbeeSleep(false);
+					state = PROGRAMMING_MODE;
+				}
+				else
+				{
+					// Pressing button 1 to 9 when powering up, select another button page
+					codeIndexOffset = (button + 1) * NUM_BUTTONS * 2;
+					state = NORMAL_MODE;
+				}
 			}
 			else
 			{
 				state = NORMAL_MODE;
 			}
 			break;
+		}
 
 		case NORMAL_MODE:
 			xbeeSleep(true);
@@ -358,7 +507,7 @@ void loop()
 			sleep_mode ();
 			sleep_disable ();
 			xbeeSleep(false);
-			checkAndReactToButtons();
+			checkAndReactToButtonChanges();
 			break;
 
 		case PROGRAMMING_MODE:
