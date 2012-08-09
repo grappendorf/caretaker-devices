a357a117f6cc241503cf44ce50fcb1bf25a004f3
Dirk Grappendorf <d.grappendorf@web.de>
2012-08-09 15:31:12 +0000

    - Updated circuit (jumpers for selecting CoYoHo/Kit mode) - First version of a three word recognition firmware

--- coyoho-device-easyvr/easyvr.cpp
+++ coyoho-device-easyvr/easyvr.cpp
@@ -17,29 +17,190 @@
  */
 
 #include <Arduino.h>
-#include <WiFlySerial/WiFlySerial.h>
 #include <SoftwareSerial/SoftwareSerial.h>
+#include <EasyVR/EasyVR.h>
+#include <XXBee/XXBee.h>
+#include <CoYoHoMessages.h>
+#include <CoYoHoListenerManager.h>
+#include <avr/wdt.h>
 
 const int PIN_LED = 10;
-const int PIN_WIFLY_RXD = 3;
-const int PIN_WIFLY_TXD = 2;
+const int PIN_XBEE_RXD = 0;
+const int PIN_XBEE_TXD = 1;
+const int PIN_USB_RXD = 2;
+const int PIN_USB_TXD = 3;
+const int PIN_EASYVR_RXD = 5;
+const int PIN_EASYVR_TXD = 4;
 
-const int USB_BAUD_RATE = 9600;
-const int WIFLY_BAUD_RATE = 9600;
+const long USB_BAUD_RATE = 9600;
+const long XBEE_BAUD_RATE = 57600;
+const long EASYVR_BAUD_RATE = 9600;
 
-WiFlySerial wifly(PIN_WIFLY_RXD, PIN_WIFLY_TXD);
+XXBee<8> xbee;
+ZBRxResponse rxResponse;
+ListenerManager<4> listenerManager(&xbee);
+//SoftwareSerial usbSerial(PIN_USB_RXD, PIN_USB_TXD);
+SoftwareSerial easyvrSerial(PIN_EASYVR_RXD, PIN_EASYVR_TXD);
+EasyVR easyvr(easyvrSerial);
+
+int wordGroup = -1;
+int wordIndex = -1;
+
+/** States of the device state engine */
+enum State
+{
+	RECOGNIZE_TRIGGER, RECOGNIZE_GROUP, RECOGNIZE_WORD
+};
+
+/** Current device mode state */
+State state = RECOGNIZE_TRIGGER;
+
+void error(int num)
+{
+	for (;;)
+	{
+		for (int i = 0; i < num; ++i)
+		{
+			digitalWrite(PIN_LED, HIGH);
+			delay(250);
+			digitalWrite(PIN_LED, LOW);
+			delay(250);
+		}
+		delay(1000);
+	}
+}
+
+void blinkLed()
+{
+	digitalWrite(PIN_LED, HIGH);
+	delay(50);
+	digitalWrite(PIN_LED, LOW);
+}
+
+void notifyListeners(uint8_t button)
+{
+	uint8_t message1[] = { COYOHO_SWITCH_READ | COYOHO_MESSAGE_NOTIFY, button, 1 };
+	listenerManager.notifyListeners(message1, sizeof(message1));
+	delay(500);
+	uint8_t message2[] = { COYOHO_SWITCH_READ | COYOHO_MESSAGE_NOTIFY, button, 0 };
+	listenerManager.notifyListeners(message2, sizeof(message2));
+}
 
 void setup()
 {
 	pinMode(PIN_LED, OUTPUT);
-	Serial.begin(USB_BAUD_RATE);
-	wifly.begin();
+	digitalWrite(PIN_LED, HIGH);
+	delay(500);
+
+	easyvrSerial.begin(EASYVR_BAUD_RATE);
+	if (! easyvr.detect())
+	{
+		error(1);
+	}
+	easyvr.setTimeout(0);
+	easyvr.setLanguage(EasyVR::ENGLISH);
+
+//	usbSerial.begin(USB_BAUD_RATE);
+
+	xbee.begin(XBEE_BAUD_RATE);
+
+	digitalWrite(PIN_LED, LOW);
+    easyvr.playSound(2, EasyVR::VOL_DOUBLE);
+	easyvr.recognizeCommand(0);
+	wdt_enable(WDTO_2S);
 }
 
 void loop()
 {
-	digitalWrite(10, HIGH);
-	delay(200);
-	digitalWrite(10, LOW);
-	delay(200);
+	wdt_reset();
+	listenerManager.checkListenerLeases();
+
+	xbee.readPacket();
+	if (xbee.getResponse().isAvailable())
+	{
+		if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
+		{
+			xbee.getResponse().getZBRxResponse(rxResponse);
+			xbee.resetData(rxResponse.getData(), rxResponse.getDataLength());
+			while (xbee.dataAvailable())
+			{
+				uint8_t command = xbee.getData();
+
+				if (listenerManager.processXBeeMessage(command, xbee, rxResponse))
+				{
+					continue;
+				}
+
+				switch (command)
+				{
+					case COYOHO_RESET:
+						digitalWrite(PIN_LED, HIGH);
+						for (;;)
+						{
+						}
+						break;
+				}
+			}
+		}
+	}
+
+	switch (state)
+	{
+		case RECOGNIZE_TRIGGER:
+			if (easyvr.hasFinished())
+			{
+				blinkLed();
+				int wordTrigger = easyvr.getCommand();
+				if (wordTrigger >= 0)
+				{
+					easyvr.recognizeCommand(1);
+					state = RECOGNIZE_GROUP;
+				}
+				else
+				{
+				    easyvr.playSound(2, EasyVR::VOL_DOUBLE);
+					easyvr.recognizeCommand(0);
+				}
+			}
+			break;
+
+		case RECOGNIZE_GROUP:
+			if (easyvr.hasFinished())
+			{
+				blinkLed();
+				wordGroup = easyvr.getCommand();
+				if (wordGroup >= 0)
+				{
+					easyvr.recognizeCommand(1 + wordGroup);
+					state = RECOGNIZE_WORD;
+				}
+				else
+				{
+				    easyvr.playSound(2, EasyVR::VOL_DOUBLE);
+					easyvr.recognizeCommand(0);
+					state = RECOGNIZE_TRIGGER;
+				}
+			}
+			break;
+
+		case RECOGNIZE_WORD:
+			if (easyvr.hasFinished())
+			{
+				blinkLed();
+				wordIndex = easyvr.getCommand();
+				if (wordIndex >= 0)
+				{
+			        notifyListeners(wordIndex - 1);
+					easyvr.recognizeCommand(0);
+					state = RECOGNIZE_TRIGGER;
+				}
+				else
+				{
+				    easyvr.playSound(2, EasyVR::VOL_DOUBLE);
+					easyvr.recognizeCommand(0);
+					state = RECOGNIZE_TRIGGER;
+				}
+			}
+			break;
+	}
 }
