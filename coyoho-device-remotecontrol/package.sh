b3b1020ab7acbdb08d79691a1a1d9ea658868929
Dirk Grappendorf <dirk@grappendorf.net>
2014-01-04 22:09:56 +0100

    New revision of the remote control PCB

--- coyoho-device-remotecontrol/package.sh
+++ coyoho-device-remotecontrol/package.sh
@@ -6,6 +6,7 @@ rm -f build/*.zip
 cd ..
 zip ${PROJECT_DIR}/build/coyoho-device-remotecontrol-$VERSION.zip ${PROJECT_DIR} -r \
 	--exclude ${PROJECT_DIR}/build/\* \
+	--exclude ${PROJECT_DIR}/eagle-v*/* \
 	--exclude ${PROJECT_DIR}/eagle/\*.b\#? \
 	--exclude ${PROJECT_DIR}/eagle/\*.s\#?
 cd ${PROJECT_DIR}
