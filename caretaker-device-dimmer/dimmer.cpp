c6a45d86ee7805670b253d025ed257de2af48a21
Dirk Grappendorf <dirk@grappendorf.net>
2015-03-06 02:25:47 +0100

    feat: Convert the XBee based Dimmer device to a WiFly device

--- caretaker-device-dimmer/dimmer.cpp
+++ caretaker-device-dimmer/dimmer.cpp
@@ -15,21 +15,15 @@
 
 #include <Arduino.h>
 #include <Bounce/Bounce.h>
-#include <XXBee/XXBee.h>
-#include <CoYoHoMessages.h>
-#include <CoYoHoListenerManager.h>
-#include <avr/wdt.h>
+#include <device.h>
 
 /** Digital I/O pin numbers */
 const int PIN_LED = 8;
 const int PIN_INT0 = 2;
-const int PIN_KEY = 7;
+const int PIN_BUTTON = 7;
 const int PIN_TRIAC = 19;
 #define PORT_TRIAC PORTC
 #define BIT_TRIAC PC5
-
-/** ZigBee baud rate */
-const long XBEE_BAUD_RATE = 57600;
 
 /** LED durations */
 const uint8_t LED_BLINK_DURATION = 100;
@@ -42,7 +36,7 @@ unsigned long ledOffMillis = 0;
 unsigned long ledNextMillis = 0;
 
 /** Key debouncer */
-Bounce key(PIN_KEY, 10);
+Bounce key(PIN_BUTTON, 10);
 
 /**
  * Maximum interval during which the key must be pressed and released to be
@@ -58,12 +52,6 @@ const unsigned long KEY_HOLD_INTERVAL = 500;
 
 /** Timestamp until the key must be released to be detected as clicked */
 unsigned long keyClickedMillis = 1000;
-
-/** XBee device */
-XXBee<8> xbee;
-
-/** ZigBee reusable RX response message */
-ZBRxResponse rxResponse;
 
 /** Triac trigger duration (1 ms) */
 const uint16_t TRIAC_TRIGGER_DURATION = 2000;
@@ -92,53 +80,102 @@ unsigned long nextBrightnessFadeMillis = 0;
 /** Brightness fade update delay */
 const unsigned long brightnesFadeDelay = 10;
 
-/** Device state listeners */
-ListenerManager<4> listenerManager(& xbee);
+/** Device configuration */
+DeviceDescriptor device;
+
+void register_message_handlers();
+void blinkLED();
+void calmDownLED();
+void configureINT0();
+void configureTimer1();
+void setBrightness(uint8_t val);
+void pwm_read();
+void pwm_write();
 
 /**
- * Notify all listeners about a dimmer value change.
+ * System setup.
  */
-void notifyListeners()
-{
-	uint8_t message[] = { COYOHO_PWM_READ | COYOHO_MESSAGE_NOTIFY, 0, brightness };
-	listenerManager.notifyListeners(message, sizeof(message));
+void setup() {
+  device.type = "Dimmer";
+  device.description = "Dimmer";
+  device.led_pin = PIN_LED;
+  device.button_pin = PIN_BUTTON;
+  device.register_message_handlers = register_message_handlers;
+  device_init(device);
+
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIAC, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT);
+  digitalWrite(PIN_BUTTON, HIGH);
+  configureINT0();
+  configureTimer1();
+}
+
+/**
+ * Main execution loop.
+ */
+void loop() {
+  device_update();
+
+  if (device_is_operational()) {
+    calmDownLED();
+
+    key.update();
+    if (key.fallingEdge()) {
+      keyClickedMillis = millis() + KEY_CLICKED_INTERVAL;
+    } else if (key.risingEdge() && millis() < keyClickedMillis) {
+      if (brightnessFadeDelta == 1) {
+        setBrightness(255);
+      } else {
+        setBrightness(0);
+      }
+    } else if (key.read() == LOW && key.duration() > KEY_HOLD_INTERVAL) {
+      if (millis() > nextBrightnessFadeMillis) {
+        setBrightness(brightness + brightnessFadeDelta);
+        nextBrightnessFadeMillis = millis() + brightnesFadeDelay;
+      }
+    }
+  }
+}
+
+/**
+ * Register device specific message handlers.
+ */
+void register_message_handlers() {
+  device.messenger->attach(MSG_PWM_WRITE, pwm_write);
+  device.messenger->attach(MSG_PWM_READ, pwm_read);
 }
 
 /**
  * Flash the LED.
  */
-void blinkLED()
-{
-	if (millis() > ledNextMillis)
-	{
-		ledOffMillis = millis() + LED_BLINK_DURATION;
-		ledNextMillis = ledOffMillis + LED_BLINK_DELAY;
-		digitalWrite(PIN_LED, HIGH);
-	}
+void blinkLED() {
+  if (millis() > ledNextMillis) {
+    ledOffMillis = millis() + LED_BLINK_DURATION;
+    ledNextMillis = ledOffMillis + LED_BLINK_DELAY;
+    digitalWrite(PIN_LED, HIGH);
+  }
 }
 
 /**
  * Switch the LED off if the blink duration elapsed.
  */
-void calmDownLED()
-{
-	if (millis() > ledOffMillis)
-	{
-		digitalWrite(PIN_LED, LOW);
-	}
+void calmDownLED() {
+  if (millis() > ledOffMillis) {
+    digitalWrite(PIN_LED, LOW);
+  }
 }
 
 /**
  * Enable timer 1, running with 16/8 = 2MHz.
  * Enable output compare A interrupt.
  */
-void configureTimer1()
-{
-	TCCR1A = 0;
-	TCCR1B = (1 << CS11);
-	OCR1A = 20000;
-	TCNT1 = 0;
-	TIMSK1 |= (1 << OCIE1A);
+void configureTimer1() {
+  TCCR1A = 0;
+  TCCR1B = (1 << CS11);
+  OCR1A = 20000;
+  TCNT1 = 0;
+  TIMSK1 |= (1 << OCIE1A);
 }
 
 /**
@@ -149,18 +186,15 @@ void configureTimer1()
  * will be triggered (resulting in darker light). The compare register is
  * then increased by TRIAC_TRIGGER_DURATION, so after this delay (1 ms)
  * this interrupt is called again. It then switches off the triac gate.
- */ISR (TIMER1_COMPA_vect)
-{
-	if (triggerTriac)
-	{
-		OCR1A += TRIAC_TRIGGER_DURATION;
-		PORT_TRIAC |= (1 << BIT_TRIAC);
-		triggerTriac = false;
-	}
-	else
-	{
-		PORT_TRIAC &= ~(1 << BIT_TRIAC);
-	}
+ */
+ISR (TIMER1_COMPA_vect) {
+  if (triggerTriac) {
+    OCR1A += TRIAC_TRIGGER_DURATION;
+    PORT_TRIAC |= (1 << BIT_TRIAC);
+    triggerTriac = false;
+  } else {
+    PORT_TRIAC &= ~(1 << BIT_TRIAC);
+  }
 }
 
 /**
@@ -170,21 +204,20 @@ void configureTimer1()
  * This interrupt is disabled by default. Only for brightness values > 0, the
  * INT0 interrupt will be enabled.
  */
-void configureINT0()
-{
-	pinMode(PIN_INT0, INPUT);
-	digitalWrite(PIN_INT0, HIGH);
-	EICRA &= ~(1 << ISC00);
-	EICRA |= (1 << ISC01);
+void configureINT0() {
+  pinMode(PIN_INT0, INPUT);
+  digitalWrite(PIN_INT0, HIGH);
+  EICRA &= ~(1 << ISC00);
+  EICRA |= (1 << ISC01);
 }
 
 /**
  * Called on each zero crossing of the 50 Hz AC power line.
- */ISR (INT0_vect)
-{
-	OCR1A = TCNT1 + triacDelay;
-	triggerTriac = true;
-	PORT_TRIAC &= ~(1 << BIT_TRIAC);
+ */
+ISR (INT0_vect) {
+  OCR1A = TCNT1 + triacDelay;
+  triggerTriac = true;
+  PORT_TRIAC &= ~(1 << BIT_TRIAC);
 }
 
 /**
@@ -192,178 +225,69 @@ void configureINT0()
  *
  * @param val The new brightnes value.
  */
-void setBrightness(uint8_t val)
-{
-	blinkLED();
-	if (val == 0)
-	{
-		triacDelay = TRIAC_DELAY_MAX;
-		EIMSK &= ~(1 << INT0);
-	}
-	else
-	{
-		triacDelay = map(val, 0, 255, TRIAC_DELAY_MAX, TRIAC_DELAY_MIN);
-		EIMSK |= (1 << INT0);
-	}
-	brightness = val;
-	if (val == 255)
-	{
-		brightnessFadeDelta = -1;
-	}
-	else if (val == 0)
-	{
-		brightnessFadeDelta = 1;
-	}
-	notifyListeners();
+void setBrightness(uint8_t val) {
+  blinkLED();
+  if (val == 0) {
+    triacDelay = TRIAC_DELAY_MAX;
+    EIMSK &= ~(1 << INT0);
+  } else {
+    triacDelay = map(val, 0, 255, TRIAC_DELAY_MAX, TRIAC_DELAY_MIN);
+    EIMSK |= (1 << INT0);
+  }
+  brightness = val;
+  if (val == 255) {
+    brightnessFadeDelta = -1;
+  } else if (val == 0) {
+    brightnessFadeDelta = 1;
+  }
+  pwm_read();
 }
 
 /**
- * System setup.
+ * Called when a MSG_PWM_WRITE was received.
  */
-void setup()
-{
-	pinMode(PIN_LED, OUTPUT);
-	pinMode(PIN_TRIAC, OUTPUT);
-	pinMode(PIN_KEY, INPUT);
-	digitalWrite(PIN_KEY, HIGH);
-	configureINT0();
-	configureTimer1();
-	Serial.begin(XBEE_BAUD_RATE);
-	xbee.begin(Serial);
-	wdt_enable(WDTO_2S);
+void pwm_write() {
+  device.messenger->next(); // Ignore pwm number
+  int mode = device.messenger->readIntArg();
+  switch (mode) {
+    case WRITE_DEFAULT:
+      setBrightness(0);
+      break;
+    case WRITE_ABSOLUTE: {
+      int value = device.messenger->readIntArg();
+      setBrightness(value);
+      break;
+    }
+    case WRITE_INCREMENT: {
+      int value = device.messenger->readIntArg();
+      setBrightness(brightness + value);
+      break;
+    }
+    case WRITE_INCREMENT_DEFAULT:
+      setBrightness(brightness + 10);
+      break;
+    case WRITE_DECREMENT: {
+      int value = device.messenger->readIntArg();
+      setBrightness(brightness - value);
+      break;
+    }
+    case WRITE_DECREMENT_DEFAULT:
+      setBrightness(brightness - 10);
+      break;
+    case WRITE_TOGGLE:
+      setBrightness(brightness != 255 ? 255 : 0);
+      break;
+    default:
+      break;
+  }
 }
 
 /**
- * Main execution loop.
+ * Called when a MSG_PWM_READ was received.
  */
-void loop()
-{
-	wdt_reset();
-	calmDownLED();
-	listenerManager.checkListenerLeases();
-
-	key.update();
-	if (key.fallingEdge())
-	{
-		keyClickedMillis = millis() + KEY_CLICKED_INTERVAL;
-	}
-	else if (key.risingEdge() && millis() < keyClickedMillis)
-	{
-		if (brightnessFadeDelta == 1)
-		{
-			setBrightness(255);
-		}
-		else
-		{
-			setBrightness(0);
-		}
-	}
-	else if (key.read() == LOW && key.duration() > KEY_HOLD_INTERVAL)
-	{
-		if (millis() > nextBrightnessFadeMillis)
-		{
-			setBrightness(brightness + brightnessFadeDelta);
-			nextBrightnessFadeMillis = millis() + brightnesFadeDelay;
-		}
-	}
-
-	xbee.readPacket();
-	if (xbee.getResponse().isAvailable())
-	{
-		if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
-		{
-			blinkLED();
-			xbee.getResponse().getZBRxResponse(rxResponse);
-			xbee.resetData(rxResponse.getData(), rxResponse.getDataLength());
-			while (xbee.dataAvailable())
-			{
-				uint8_t command = xbee.getData();
-
-				if (listenerManager.processXBeeMessage(command, xbee, rxResponse))
-				{
-					continue;
-				}
-
-				switch (command)
-				{
-					case COYOHO_RESET:
-						for (;;)
-						{
-						}
-						break;
-
-					case COYOHO_PWM_WRITE:
-						if (xbee.dataAvailable(2))
-						{
-							uint8_t pwmNum = xbee.getData();
-							uint8_t mode = xbee.getData();
-							if (pwmNum != 0)
-							{
-								break;
-							}
-							switch (mode)
-							{
-								case COYOHO_WRITE_ABSOLUTE:
-									if (xbee.dataAvailable())
-									{
-										setBrightness(xbee.getData());
-									}
-									break;
-								case COYOHO_WRITE_INCREMENT:
-									if (xbee.dataAvailable())
-									{
-										setBrightness(brightness + xbee.getData());
-									}
-									break;
-								case COYOHO_WRITE_INCREMENT_DEFAULT:
-									setBrightness(brightness + 10);
-									break;
-								case COYOHO_WRITE_DECREMENT:
-									if (xbee.dataAvailable())
-									{
-										setBrightness(brightness - xbee.getData());
-									}
-									break;
-								case COYOHO_WRITE_DECREMENT_DEFAULT:
-									setBrightness(brightness - 10);
-									break;
-								case COYOHO_WRITE_TOGGLE:
-									setBrightness(brightness != 255 ? 255 : 0);
-									break;
-								case COYOHO_WRITE_DEFAULT:
-								default:
-									setBrightness(0);
-									break;
-							}
-						}
-						break;
-
-					case COYOHO_PWM_READ:
-						if (xbee.dataAvailable(1))
-						{
-							uint8_t pwmNum = xbee.getData();
-							if (pwmNum != 0)
-							{
-								break;
-							}
-							xbee.resetPayload();
-							xbee.putPayload(COYOHO_PWM_READ | COYOHO_MESSAGE_RESPONSE);
-							xbee.putPayload(0);
-							xbee.putPayload(brightness);
-							ZBTxRequest txRequest(rxResponse.getRemoteAddress64(), xbee.payload(),
-									xbee.payloadLenght());
-							xbee.send(txRequest);
-						}
-						break;
-
-					case COYOHO_DUMP:
-						xbee.resetPayload();
-						xbee.putPayload(COYOHO_DUMP | COYOHO_MESSAGE_RESPONSE);
-						xbee.putPayload(COYOHO_DUMP_VERSION);
-						xbee.putPayload(COYOHO_VERSION);
-						break;
-				}
-			}
-		}
-	}
+void pwm_read() {
+  device.messenger->sendCmdStart(MSG_PWM_STATE);
+  device.messenger->sendCmdArg(0);
+  device.messenger->sendCmdArg(brightness);
+  device.messenger->sendCmdEnd();
 }
