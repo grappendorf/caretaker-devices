d22d5b2ee03ecbbdb7b19a725b4efe322f17ffe1
Dirk Grappendorf <dirk@grappendorf.net>
2015-03-12 12:09:17 +0100

    doc: Fix file header

--- caretaker-device-dimmerrgb/dimmerrgb.cpp
+++ caretaker-device-dimmerrgb/dimmerrgb.cpp
@@ -1,19 +1,10 @@
 /**
- * This file is part of the CoYoHo Control Your Home System.
+ * This file is part of the Caretaker Home Automation System
  *
- * Copyright 2011-2012 Dirk Grappendorf, www.grappendorf.net
+ * Copyright 2011-2015 Dirk Grappendorf, www.grappendorf.net
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed under the the MIT License
+ * You find a copy of license in the root directory of this project
  */
 
 #include <Arduino.h>
