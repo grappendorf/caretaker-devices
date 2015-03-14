512125f96a019a342d4ab585bb6cb2cf3073469d
Dirk Grappendorf <dirk@grappendorf.net>
2015-03-14 15:33:45 +0100

    chore: Add missing project files

---
+++ coyoho-device-easyvr/build/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../easyvr.cpp 
+
+OBJS += \
+./easyvr.o 
+
+CPP_DEPS += \
+./easyvr.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"/home/grappendorf/workspace-arduino/arduino-framework" -I"/home/grappendorf/workspace-coyoho/coyoho-devices/coyoho-device-base" -Wall -Os -ffunction-sections -fdata-sections -fno-exceptions -ffunction-sections -fdata-sections -mmcu=atmega168 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
