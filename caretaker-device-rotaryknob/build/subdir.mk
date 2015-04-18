a28aa453d7ec9cae471368539e43bf2cde21a156
Dirk Grappendorf <dirk@grappendorf.net>
2015-04-18 13:38:46 +0200

    feat: Add a rotary knob device

---
+++ caretaker-device-rotaryknob/build/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../rotaryknob.cpp 
+
+OBJS += \
+./rotaryknob.o 
+
+CPP_DEPS += \
+./rotaryknob.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"/home/grappendorf/workspace-arduino/arduino-framework" -I"/home/grappendorf/workspace-caretaker/caretaker-devices/caretaker-device" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -funsigned-char -funsigned-bitfields -fno-exceptions -mmcu=atmega328p -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
